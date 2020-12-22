e3a64e24a62cf56e80a1c555c0a6295e41a907a7
Brian Muturi <muturibrian18@gmail.com>
2020-12-22 01:54:40 +0100

    Add project files.

---
+++ Drawing a recttangle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Drawing_a_rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter X:");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Enter Y:");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("The rectangle is of dimensions" + (x) + "by" + (y));
+            for(int i=0; i<(x); i++)
+            {
+                for (int j = 0; j < (y); j++)
+                    Console.Write("*");
+                Console.WriteLine("*");
+
+            }
+        }
+    }
+}
